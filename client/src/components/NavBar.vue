fd359a8b99dab174b8eae86d08c0902f2d7fb548
Zeyu Shao <zeyu.shao@hotmail.com>
2024-11-17 23:22:47 -0500

    updated some package.json and Navbar styling

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <b-navbar toggleable="lg" type="dark" variant="white">
+    <b-navbar class="lexend sticky-top" toggleable="lg" type="dark" variant="white">
         <b-navbar-brand class="brand hover-text-pink" href="#">
             <div class="d-flex align-items-center">
                 <img class="logo" src="../assets/logo.png" alt="ACAO Logo">
@@ -14,16 +14,20 @@
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
         <b-collapse id="nav-collapse" class="nav-items-container" is-nav>
-            <b-navbar-nav class="ml-auto">
-                <b-nav-item class="custom-nav-item" href="">Our Events</b-nav-item>
-                <b-nav-item href="">Meet the Team</b-nav-item>
-                <b-nav-item href="">Photo Gallery</b-nav-item>
+            <b-navbar-nav>
+                <b-nav-item class="fs-5 brand" href="">Our Events</b-nav-item>
+                <b-nav-item class="fs-5 brand" href="">Meet the Team</b-nav-item>
+                <b-nav-item class="fs-5 brand" href="">Photo Gallery</b-nav-item>
             </b-navbar-nav>
         </b-collapse>
     </b-navbar>
 </template>
 
 <style scoped>
+.lexend {
+    font-family: 'Lexend', sans-serif;
+}
+
 .brand {
     margin-left: 2vw;
 }
