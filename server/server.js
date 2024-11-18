1c8210c32391c7f32314a8008d6c40fd364cc647
Zeyu Shao <zeyu.shao@hotmail.com>
2024-11-17 21:02:20 -0500

    fixed package.json in root for heroku

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const path = require('path');
 
 dotenv.config(); // To load environment variables from .env file
 
