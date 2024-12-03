6d1d1254eaa45dc591754b20113d03ebe288f773
Zeyu Shao <zeyu.shao@hotmail.com>
2024-12-03 03:06:22 -0500

    KAN-17 completed static version of events page and updated authentication to validate token every time page is accessed

--- client/src/components/EventsPage.vue
+++ client/src/components/EventsPage.vue
@@ -4,13 +4,48 @@
 
 <template>
     <div id="events">
-
+        <BContainer class="pt-5">
+            <BRow class="pb-5">
+                <BCol md="8">
+                    <h1>Our Upcoming Event</h1>
+                </BCol>
+                <BCol md="4"></BCol>
+            </BRow>
+            <BRow class="d-flex">
+                <BCol md="8" class="d-flex justify-content-center align-items-center">
+                    <BCard
+                    id="event-image"
+                    img-src=""
+                    img-alt="Image"
+                    img-top
+                    tag="article"
+                    />
+                </BCol>
+                <BCol md="4" class="d-flex flex-column justify-content-center align-items-center">
+                    <BRow class="">
+                        <h3>Event Title</h3>
+                    </BRow>
+                    <BRow class="">
+                        <p>Event details</p>
+                    </BRow>
+                    <BRow class="">
+                        <BButton>Register Now</BButton>
+                    </BRow>
+                </BCol>
+            </BRow>
+        </BContainer>
     </div>
 </template>
 
 <style scoped>
 #events {
     background-color: #FFF4E9;
-    height: 1080px;
+    height: 100vh;
+}
+
+#event-image {
+    background-color: white;
+    height: 50vh;
+    width: 50vh;
 }
 </style>
